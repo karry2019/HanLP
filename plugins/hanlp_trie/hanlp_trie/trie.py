c3a5557643e50b3b45c489ea1caf02058e7c4bfb
崔志明 <cuizhiming@hotmail.com>
2021-04-23 09:31:14 +0800

    TEMP

--- plugins/hanlp_trie/hanlp_trie/trie.py
+++ plugins/hanlp_trie/hanlp_trie/trie.py
@@ -4,6 +4,7 @@
 from typing import Dict, Any, List, Tuple, Sequence, Union, Iterable, Optional
 
 
+# 节点（Node）三要素：对应边（key）、自己的子节点（字典：_children）、是否代表一个词（用value标识，暂时让value=None，表示不代表一个词）
 class Node(object):
     def __init__(self, value=None) -> None:
         """A node in a trie tree.
@@ -11,10 +12,17 @@ class Node(object):
         Args:
             value: The value associated with this node.
         """
-        self._children = {}
+        self._children = {}  # 词典结构：字典键:边所对应的单字（字符）；字典值：子节点对象（Node实例）#节点所持有的数据（用于标识是否是终结点 / 是否代表一个词）
         self._value = value
 
+    # 需要添加的字典点所属的边值（char）如果在当前节点中存在，就考虑复写；如果不存在，就根据char和value的值往当前节点的子节点字典中添加一个新的子节点
+    # 添加子节点需要一个字符一个字符的添加
     def _add_child(self, char, value, overwrite=False):
+        """
+        Args:
+            char: 需要添加的子节点的边对应的字符，即字典self._children中的键
+            value: 用于标识添加的子节点是否是一个词的终点，None表示非；即字典self._children中的值（Node对象）所包含的_value值
+        """
         child = self._children.get(char)
         if child is None:
             child = Node(value)
@@ -23,6 +31,7 @@ class Node(object):
             child._value = value
         return child
 
+    # 确定有限状态自动机：key-需要查询的字符串（词）；返回结果如果为None，则说明为查询到对应的节点（不是词），否则反之
     def transit(self, key):
         """Transit the state of a Deterministic Finite Automata (DFA) with key.
 
@@ -66,26 +75,30 @@ class Trie(Node):
                     self[k] = v
             else:
                 for k in tokens:
-                    self[k] = True
+                    self[k] = True  # True会复制到终点子节点上
 
+    # 这里的key代表的是需要查的词（一到多个字符不等）
     def __contains__(self, key):
         return self[key] is not None
 
+    # 获取经过状态转移之后的节点所对应的value值，如果为None，则说明查询的词不存在（因为我们约定，None表示查询的词不存在）
     def __getitem__(self, key):
         state = self.transit(key)
         if state is None:
             return None
         return state._value
 
+    # key-需要添加的字符串（词），value-对应的节点上的值，约定None代表该节点不是一个词的终点
     def __setitem__(self, key, value):
         state = self
         for i, char in enumerate(key):
             if i < len(key) - 1:
-                state = state._add_child(char, None, False)
+                state = state._add_child(char, None, False)  # 路径子节点
             else:
-                state = state._add_child(char, value, True)
-        self._size += 1
+                state = state._add_child(char, value, True)  # 终点子节点
+        self._size += 1  # 字典树词数量累加
 
+    # 并不是真正的删除操作，属于逻辑删除范畴，删除一个词，说白了就是把终节点上的value值置空，让_value=None
     def __delitem__(self, key):
         state = self.transit(key)
         if state is not None:
